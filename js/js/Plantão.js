632e346bf415a69513d17ae351070943d284e5d0
adielsilvastefanini-spec <adielsilva.stefanini@latam.com>
2026-09-09 11:56:11 -0300

    Create Plantão.js

---
+++ js/js/Plantão.js
@@ -0,0 +1,8 @@
+// Módulo de Plantão
+function carregarPlantao() {
+  console.log("Módulo de Plantão carregado com sucesso.");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  carregarPlantao();
+});
