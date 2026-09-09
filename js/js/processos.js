298304608a4d5a9ad396606988e9db3cbfab59df
adielsilvastefanini-spec <adielsilva.stefanini@latam.com>
2026-09-09 11:55:41 -0300

    Create processos.js

---
+++ js/js/processos.js
@@ -0,0 +1,8 @@
+// Módulo de Processos
+function carregarProcessos() {
+  console.log("Módulo de Processos carregado com sucesso.");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  carregarProcessos();
+});
