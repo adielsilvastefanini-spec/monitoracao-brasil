2f61ea62e86025c6562556eb57f47e8ce6dfdb51
adielsilvastefanini-spec <adielsilva.stefanini@latam.com>
2026-09-09 11:51:20 -0300

    Create escalonamento.js

---
+++ js/escalonamento.js
@@ -0,0 +1,9 @@
+// Módulo de Escalonamento
+function carregarEscalonamento() {
+  console.log("Módulo de Escalonamento carregado com sucesso.");
+}
+
+// Inicialização automática ao carregar o script
+document.addEventListener("DOMContentLoaded", function () {
+  carregarEscalonamento();
+});
